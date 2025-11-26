699e9e87684e31cbc3c95ab7d16a9eb3b5be844d
miche <michelgit23@hotmail.com>
2025-11-26 18:43:37 -0300

    1° commit.

---
+++ qrcode-api/src/test/java/br/com/michel/qrcode_api/QrcodeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.michel.qrcode_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrcodeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

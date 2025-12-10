fa1f79d008a7810a0433fe83c77f51096ac7a8d5
Vinicius <viniciusmartins-@live.com>
2025-12-09 21:10:25 -0300

    first commit: estrutura projeto

---
+++ src/main/java/com/github/michelsaine/qrcode_api/controller/QrCodeController.java
@@ -0,0 +1,8 @@
+package com.github.michelsaine.qrcode_api.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class QrCodeController {
+
+}
---
+++ src/main/java/com/github/michelsaine/qrcode_api/model/QrCode.java
@@ -0,0 +1,7 @@
+package com.github.michelsaine.qrcode_api.model;
+
+public record QrCode(
+        String valor,
+        String descricao,
+        String status
+) {}
